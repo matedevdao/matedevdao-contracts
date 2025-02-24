d7baef745c950835243da977a0875b086de7fdf3
yjgaia <yjdev88@gmail.com>
2025-02-24 12:20:31 +0900

    Update OpenZeppelin contracts version and fix import path for ReentrancyGuard

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
